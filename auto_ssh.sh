7f335ae4651b250e57ad15f56074b61c2002fe37
Moonlor <Kenneth0875@gmail.com>
2018-11-27 12:59:57 +0800

    [Add] Add port selection

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -3,9 +3,10 @@ set timeout 10
 set username [lindex $argv 0]
 set password [lindex $argv 1]
 set hostname [lindex $argv 2]
-spawn ssh-copy-id -i /root/.ssh/id_rsa.pub $username@$hostname
+set port [lindex $argv 3]
+spawn ssh-copy-id -i /root/.ssh/id_rsa.pub -p $port $username@$hostname
 expect {
-#first connect, no public key in ~/.ssh/known_hosts
+    #first connect, no public key in ~/.ssh/known_hosts
     "Are you sure you want to continue connecting (yes/no)?" {
         send "yes\r"
         expect "password:"
