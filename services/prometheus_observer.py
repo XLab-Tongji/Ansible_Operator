b1d3d74f515744fa8a84c4a52d8297a092fb9517
tjuyinkanglin <14_ykl@tongji.edu.cn>
2018-12-21 17:21:31 +0800

    [FIX]fix read time error

--- services/prometheus_observer.py
+++ services/prometheus_observer.py
@@ -59,10 +59,20 @@ class PrometheusObserver(object):
         return csvset
 
     @staticmethod
+    def datetime_timestamp(dt):
+        # dt为字符串
+        # 中间过程，一般都需要将字符串转化为时间数组
+        time.strptime(dt, '%Y-%m-%d %H:%M:%S')
+        ## time.struct_time(tm_year=2012, tm_mon=3, tm_mday=28, tm_hour=6, tm_min=53, tm_sec=40, tm_wday=2, tm_yday=88, tm_isdst=-1)
+        # 将"2012-03-28 06:53:40"转化为时间戳
+        s = time.mktime(time.strptime(dt, '%Y-%m-%d %H:%M:%S'))
+        return int(s)
+
+    @staticmethod
     def run(dto):
         # 处理开始时间和结束时间
-        end_time = datetime.datetime.fromtimestamp(dto['from'])
-        start_time = datetime.datetime.fromtimestamp(dto['to'])
+        end_time = PrometheusObserver.datetime_timestamp(dto['from'])
+        start_time = PrometheusObserver.datetime_timestampdto(['to'])
 
         metricnames1 = PrometheusObserver.build_entity_metrics(Config.QUERY_CONFIG1)
         csvset1 = PrometheusObserver.query_entity_metric_values(
