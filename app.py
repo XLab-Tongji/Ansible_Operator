bc661b6e0e79899fb945815512ad9ef93603040c
Moonlor <kenneth0875@gmail.com>
2019-04-04 17:16:08 +0800

    [UPDATE] Remove authentication

--- app.py
+++ app.py
@@ -28,37 +28,36 @@ def unauthorized():
 
 
 @app.errorhandler(404)
-@auth.login_required
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.route('/tool/api/v1.0/get_namespace', methods=['GET'])
-@auth.login_required
+# @auth.login_required
 def get_namespace():
     return jsonify(K8sObserver.get_namespace())
 
 
 @app.route('/tool/api/v1.0/get_svc/<namespace>', methods=['GET'])
-@auth.login_required
+# @auth.login_required
 def get_svc(namespace):
     return jsonify(K8sObserver.get_svc(namespace=namespace.encode('raw_unicode_escape')))
 
 
 @app.route('/tool/api/v1.0/get_deployment/<namespace>', methods=['GET'])
-@auth.login_required
+# @auth.login_required
 def get_deployment(namespace):
     return jsonify(K8sObserver.get_deployment(namespace=namespace.encode('raw_unicode_escape')))
 
 
 @app.route('/tool/api/v1.0/get_pods/<namespace>', methods=['GET'])
-@auth.login_required
+# @auth.login_required
 def get_pods(namespace):
     return jsonify(K8sObserver.get_pods(namespace))
 
 
 @app.route('/tool/api/v1.0/prometheus/result/stream', methods=['POST'])
-@auth.login_required
+# @auth.login_required
 def stream():
     if not request.json or not 'from' in request.json or not 'to' in request.json:
         abort(400)
